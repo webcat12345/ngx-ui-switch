b62928d17dfbb80d619abc102caf351c9bf43b6e
Chris McKnight <cmckni3@gmail.com>
2020-02-25 18:12:03 -0600

    fix(component): Fix whitespace

--- src/lib/ui-switch/ui-switch.component.ts
+++ src/lib/ui-switch/ui-switch.component.ts
@@ -5,7 +5,10 @@ import {
   Output,
   EventEmitter,
   HostListener,
-  forwardRef, Inject, Optional, OnDestroy,
+  forwardRef,
+  Inject,
+  Optional,
+  OnDestroy,
 } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 import { UI_SWITCH_OPTIONS } from './ui-switch.token';
@@ -22,23 +25,34 @@ const UI_SWITCH_CONTROL_VALUE_ACCESSOR: any = {
 @Component({
   selector: 'ui-switch',
   template: `
-    <button 
-    class="switch"
-    role="switch"
-    [attr.aria-checked]="checked"
-    [class.checked]="checked"
-    [class.disabled]="disabled"
-    [class.loading]="loading"
-    [class.switch-large]="size === 'large'"
-    [class.switch-medium]="size === 'medium'"
-    [class.switch-small]="size === 'small'"
-    [style.background-color]="getColor()"
-    [style.border-color]="getColor('borderColor')">
-    <label class="switch-pane" *ngIf="checkedLabel || uncheckedLabel">
-    <span [attr.aria-label]="this.checkedLabel" class="switch-label-checked" [style.color]="getColor('checkedTextColor')">{{ this.checkedLabel }}</span>
-    <span [attr.aria-label]="this.uncheckedLabel" class="switch-label-unchecked" [style.color]="getColor('uncheckedTextColor')">{{ this.uncheckedLabel }}</span>
-    </label>
-    <small [style.background]="getColor('switchColor')"><ng-content></ng-content></small>
+    <button
+      class="switch"
+      role="switch"
+      [attr.aria-checked]="checked"
+      [class.checked]="checked"
+      [class.disabled]="disabled"
+      [class.loading]="loading"
+      [class.switch-large]="size === 'large'"
+      [class.switch-medium]="size === 'medium'"
+      [class.switch-small]="size === 'small'"
+      [style.background-color]="getColor()"
+      [style.border-color]="getColor('borderColor')"
+    >
+      <label class="switch-pane" *ngIf="checkedLabel || uncheckedLabel">
+        <span
+          [attr.aria-label]="this.checkedLabel"
+          class="switch-label-checked"
+          [style.color]="getColor('checkedTextColor')"
+          >{{ this.checkedLabel }}</span
+        >
+        <span
+          [attr.aria-label]="this.uncheckedLabel"
+          class="switch-label-unchecked"
+          [style.color]="getColor('uncheckedTextColor')"
+          >{{ this.uncheckedLabel }}</span
+        >
+      </label>
+      <small [style.background]="getColor('switchColor')"><ng-content></ng-content></small>
     </button>
   `,
   providers: [UI_SWITCH_CONTROL_VALUE_ACCESSOR],
@@ -117,7 +131,7 @@ export class UiSwitchComponent implements ControlValueAccessor, OnDestroy {
     @Inject(UI_SWITCH_OPTIONS) @Optional() config: UiSwitchModuleConfig = {},
     private cdr: ChangeDetectorRef
   ) {
-    this.size = config && config.size || 'medium';
+    this.size = (config && config.size) || 'medium';
     this.color = config && config.color;
     this.switchOffColor = config && config.switchOffColor;
     this.switchColor = config && config.switchColor;
@@ -172,7 +186,9 @@ export class UiSwitchComponent implements ControlValueAccessor, OnDestroy {
   onToggle(event: MouseEvent) {
     if (this.beforeChange) {
       this._beforeChange = this.beforeChange.subscribe((confirm: boolean) => {
-        if (confirm) { this.onClick(event); }
+        if (confirm) {
+          this.onClick(event);
+        }
       });
     } else {
       this.onClick(event);
@@ -200,8 +216,8 @@ export class UiSwitchComponent implements ControlValueAccessor, OnDestroy {
     this.disabled = isDisabled;
   }
 
-  private onTouchedCallback = (v: any) => { };
-  private onChangeCallback = (v: any) => { };
+  private onTouchedCallback = (v: any) => {};
+  private onChangeCallback = (v: any) => {};
 
   ngOnDestroy() {
     if (this._beforeChange) {

b9c444f45255d2737145898788c9b06735cfe5cc
cmckni3 <cmckni3@gmail.com>
2022-02-02 01:10:39 +0000

    Deploying to gh-pages from @ webcat12345/ngx-ui-switch@290544765054bde3524265c41ca3e3b1503674be 🚀

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"1":"External module"},"rows":[{"id":0,"kind":1,"name":"\"public_api\"","url":"modules/_public_api_.html","classes":"tsd-kind-external-module"}]};

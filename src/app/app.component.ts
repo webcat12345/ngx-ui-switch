a5fc738af2cf25921d3d45d27d27776ff94364c7
Chris McKnight <cmckni3@gmail.com>
2024-11-01 17:06:18 -0500

    feat(demo): Reorganize demo page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,6 @@ import json from '../lib/package.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [
-    `
-      :host {
-        width: 80%;
-        display: block;
-        margin: 0 auto;
-        height: 600px;
-      }
-    `,
-  ],
 })
 export class AppComponent {
   constructor(private t: Title) {
